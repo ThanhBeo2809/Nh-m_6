f6d2a48accafa796a18f449c6c388ca29e4ce1e2
CuongDo2010 <24100332@st.phenikaa-uni.edu.vn>
2026-06-18 01:05:39 +0000

    Add library-management as regular folder (moved nested .git to .git.backup)

---
+++ library-management/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '201006',
+      database: 'QuanLyThuVien',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    BooksModule,
+  ],
+})
+export class AppModule {}
